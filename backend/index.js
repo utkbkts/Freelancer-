cdfb1957af586c3c55286aa1b4c7ecf8d6b494bb
utku <utkbktss5@gmail.com>
2024-06-04 23:31:56 +0300

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,9 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.resolve(__dirname, "/frontend/dist/index.html"));
 });
 app.use(cors());
 process.on("uncaughtException", (err) => {
