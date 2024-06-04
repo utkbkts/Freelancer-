d6fe1253a6f1eb111b92422cc0a1161b4fb8f8dd
utku <utkbktss5@gmail.com>
2024-06-04 23:35:44 +0300

    update backend

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
-});
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+// });
 app.use(cors());
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
